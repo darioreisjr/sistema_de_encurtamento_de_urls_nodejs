bb14088b19a5f00288cf2363aeb123115e89852d
Dario Reis <guttyerresjunior1995@gmail.com>
2024-03-25 07:02:11 -0300

    Feat: Adding fastify dependency, configuring typescript file and creating and configuring first route

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/teste', () => {
+  return 'Hello World!'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running on http://localhost:3333')
+})
